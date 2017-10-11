241580f44edd937c43f2dd4e092a919288b15f37
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-10-11 21:45:56 +0800

    Correcting checkstyles

--- src/main/java/seedu/address/model/Photo.java
+++ src/main/java/seedu/address/model/Photo.java
@@ -1,9 +1,9 @@
 package seedu.address.model;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 
 /**
  * Represents a Photo in the address book.
@@ -11,16 +11,19 @@ import java.io.IOException;
 
 public class Photo {
 
-private Image photo;
+    private Image photo;
 
-public Photo(String filepath) throws IOException {
-    try {
-        File sourceimage = new File(filepath);
-        this.photo = ImageIO.read(sourceimage);
+    public Photo(String filepath) throws IOException {
+        try {
+            File sourceimage = new File(filepath);
+            this.photo = ImageIO.read(sourceimage);
+        } catch(IOException e) {
+            System.out.println("Not a valid filepath");
+        }
+    
     }
-    catch(IOException e){
-        System.out.println("Not a valid filepath");
+    public Image getPhoto() { 
+        return photo; 
     }
 }
-public Image getPhoto(){return photo;}
-}
+
