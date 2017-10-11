a2ecaa6f63e965440a795b6eb45db597f344546c
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-10-11 20:36:46 +0800

    deleted trailing whitespace AGAIN.2

--- src/main/java/seedu/address/logic/parser/AddPhotoCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPhotoCommandParser.java
@@ -31,5 +31,7 @@ public class AddPhotoCommandParser implements Parser<AddPhotoCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPhotoCommand.MESSAGE_USAGE));
 
 
-        } }
+        } 
+    }
+    
 }
