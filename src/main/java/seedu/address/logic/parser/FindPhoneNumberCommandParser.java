4bae6a8ed493d329d9598937f8f93ef5b0577d5e
YanWC <e0015002@u.nus.edu>
2017-10-16 13:11:38 +0800

    Added FindPhoneNumberCommand

---
+++ src/main/java/seedu/address/logic/parser/FindPhoneNumberCommandParser.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindPhoneNumberCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NumberContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class FindPhoneNumberCommandParser implements Parser<FindPhoneNumberCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns an FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindPhoneNumberCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new FindPhoneNumberCommand(new NumberContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+}
