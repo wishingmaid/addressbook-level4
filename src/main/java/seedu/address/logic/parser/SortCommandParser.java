9e238c265850bb20bb2da15c845fff92bec799d0
ykt33 <vikingpixie@gmail.com>
2017-10-17 15:21:40 +0800

    proper checkstyle

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -12,6 +12,9 @@ import java.util.function.Consumer;
 import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new SortCommand object
+ */
 public class SortCommandParser implements Parser<SortCommand> {
 
     private String parameter;
