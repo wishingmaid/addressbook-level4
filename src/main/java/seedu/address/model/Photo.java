984c8f76027f0ef463f57ad6294d953764d1837e
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-10-20 19:50:35 +0800

    corrected checkstyle 2.

--- src/main/java/seedu/address/model/Photo.java
+++ src/main/java/seedu/address/model/Photo.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.net.MalformedURLException;
 
 import javafx.scene.image.Image;
-import seedu.address.commons.exceptions.IllegalValueException;
 
 /**
  * Represents a Photo in the address book.
@@ -12,25 +11,25 @@ import seedu.address.commons.exceptions.IllegalValueException;
 
 public class Photo {
 
-    public static final String URL_VALIDATION= "The filepath URL does not exist.";
+    public static final String URL_VALIDATION = "The filepath URL does not exist.";
     private static final String DEFAULT_PHOTOURL = "/images/noPhoto.png";
     private Image image;
     private String filepath;
     public Photo(String filepath) throws IllegalArgumentException {
         //this is to setup the default photo for contacts after it is added.
         if (filepath.equals("/images/noPhoto.png")) {
-        this.filepath = DEFAULT_PHOTOURL;
-        this.image = new Image(getClass().getResource(this.filepath).toExternalForm());
-    } else {
-        this.filepath = filepath;
-        File file = new File(filepath);
+            this.filepath = DEFAULT_PHOTOURL;
+            this.image = new Image(getClass().getResource(this.filepath).toExternalForm());
+        } else {
+            this.filepath = filepath;
+            File file = new File(filepath);
         if (isValidFilePath(file)) {
-            try {
-                String localUrl = file.toURI().toURL().toString();
-                this.image = new Image(localUrl);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
+                try {
+                    String localUrl = file.toURI().toURL().toString();
+                    this.image = new Image(localUrl);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
         } else {
             throw new IllegalArgumentException(URL_VALIDATION);
         }
